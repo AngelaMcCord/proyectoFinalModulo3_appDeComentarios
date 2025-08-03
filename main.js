459104f77d3b89e95b670f67e5c000b695c6b36a
AngelaMc <angela.corderoconde@gmail.com>
2025-08-03 01:13:42 -0400

    Añade propiedad de fecha y hora en cada nuevo comentario

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ addButton.addEventListener("click", function(e){
     e.preventDefault();
 
     if (input.value.trim() !== "") {
-
+        const now = new Date();
+        const datetime = now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
         let commentItem = `<div class="comment-card">
-                                <img class="user-photo" src="https://cdn-icons-png.flaticon.com/512/706/706807.png" alt="userphoto">
-                                ${input.value}
-                                <button class="btn-trash" ><i class="fas fa-trash"></i></button>
-                            </div>`; //template del nuevo comentario
+                                <div class="comment-section">
+                                    <img class="user-photo" src="https://cdn-icons-png.flaticon.com/512/706/706807.png" alt="userphoto">
+                                    ${input.value}
+                                    <button class="btn-trash" ><i class="fas fa-trash"></i></button>
+                                </div>
+                                <div class="comment-datetime">
+                                    <i class="far fa-clock"></i> <p>${datetime}</p>
+                                </div>
+                            </div>`; //template del nuevo comentario con la fecha y hora
         
         let commentListHtml = commentList.innerHTML;//obtener los comentarios anteriores
         commentListHtml = commentListHtml.concat(commentItem); // agregar el nuevo comentario
@@ -21,4 +27,3 @@ addButton.addEventListener("click", function(e){
     }
 }
 );
-
